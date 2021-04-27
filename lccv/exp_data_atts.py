b2d168b8417718de88ea99ce57e44170b4baf201
janvanrijn <janvanrijn@gmail.com>
2021-04-27 23:55:49 +0200

    extended experimental file

--- lccv/exp_data_atts.py
+++ lccv/exp_data_atts.py
@@ -15,11 +15,32 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--output_directory', type=str, help='directory to store output',
                         default=os.path.expanduser('~') + '/experiments/lccv/')
-    parser.add_argument('--task_idx', type=int)
+    parser.add_argument('--job_idx', type=int, default=1)
     parser.add_argument('--verbose', type=bool, default=False)
-    parser.add_argument('--study_id', type=str, default=218)
+    parser.add_argument('--study_id', type=str, default=271)
 
     return parser.parse_args()
+
+
+learners = [
+    sklearn.svm.LinearSVC,
+    sklearn.tree.DecisionTreeClassifier,
+    sklearn.tree.ExtraTreeClassifier,
+    sklearn.linear_model.LogisticRegression,
+    sklearn.linear_model.PassiveAggressiveClassifier,
+    sklearn.linear_model.Perceptron,
+    sklearn.linear_model.RidgeClassifier,
+    sklearn.linear_model.SGDClassifier,
+    sklearn.neural_network.MLPClassifier,
+    sklearn.discriminant_analysis.LinearDiscriminantAnalysis,
+    sklearn.discriminant_analysis.QuadraticDiscriminantAnalysis,
+    sklearn.naive_bayes.BernoulliNB,
+    sklearn.naive_bayes.MultinomialNB,
+    sklearn.neighbors.KNeighborsClassifier,
+    sklearn.ensemble.ExtraTreesClassifier,
+    sklearn.ensemble.RandomForestClassifier,
+    sklearn.ensemble.GradientBoostingClassifier,
+]
 
 
 def highest_2power_below(n) -> int:
@@ -27,9 +48,36 @@ def highest_2power_below(n) -> int:
     return int(pow(2, p) / 9 * 10)
 
 
-def run_on_task(
+def clf_as_pipeline(clf, numeric_indices, nominal_indices):
+        numeric_transformer = sklearn.pipeline.make_pipeline(
+            sklearn.preprocessing.SimpleImputer(),
+            sklearn.preprocessing.StandardScaler())
+
+        # note that the dataset is encoded numerically, hence we can only impute
+        # numeric values, even for the categorical columns.
+        categorical_transformer = sklearn.pipeline.make_pipeline(
+            sklearn.impute.SimpleImputer(strategy='constant', fill_value=-1),
+            sklearn.preprocessing.OneHotEncoder(handle_unknown='ignore'))
+
+        transformer = sklearn.compose.ColumnTransformer(
+            transformers=[
+                ('numeric', numeric_transformer, numeric_indices),
+                ('nominal', categorical_transformer, nominal_indices)],
+            remainder='passthrough')
+
+        pipeline = sklearn.pipeline.make_pipeline(transformer,
+                                                  sklearn.feature_selection.VarianceThreshold(),
+                                                  clf)
+        return pipeline
+
+
+def run_classifier_on_task(
+        learner_idx: int,
         task: openml.tasks.OpenMLSupervisedTask,
         output_directory: str, verbose: bool):
+    nominal_indices = task.get_dataset().get_features_by_type('nominal', [task.target_name])
+    numeric_indices = task.get_dataset().get_features_by_type('numeric', [task.target_name])
+    clf = clf_as_pipeline(learners[learner_idx], numeric_indices, nominal_indices)
     x, y = task.get_X_and_y(dataset_format='array')
     size_big = highest_2power_below(len(x))
 
@@ -40,11 +88,14 @@ def run_on_task(
 
     output_dir = os.path.join(output_directory, str(task.task_id))
     os.makedirs(output_dir, exist_ok=True)
+    filename = 'result_%s.json' % str(clf)
+    if os.path.isfile(os.path.join(output_dir, filename)):
+        logging.info('clf %s on dataset %s already exists' % (str(clf), task.get_dataset().name))
+        return
 
     logging.info('dataset: %s, shape: %s > %s > %s' % (task.get_dataset().name,
                                                        x.shape, x_big.shape,
                                                        x_small.shape))
-    clf = sklearn.ensemble.RandomForestClassifier()
     results_lccv = lccv.lccv(clf, x_small, y_small,
                              enforce_all_anchor_evaluations=True, verbose=verbose)
     prediction = results_lccv[3].get_ipl_estimate_at_target(size_big)
@@ -73,30 +124,33 @@ def run_on_task(
             int(size_big): prediction
         }
     }
-    with open(os.path.join(output_dir, 'result.json'), 'w') as fp:
+    with open(os.path.join(output_dir, filename), 'w') as fp:
         json.dump(all_results, fp)
 
 
 def run(args):
     suite = openml.study.get_suite(args.study_id)
-    if args.task_idx is not None:
-        task = openml.tasks.get_task(suite.tasks[args.task_idx])
+    if args.job_idx is not None:
+        task_idx = int(args.job_idx / len(learners))
+        learner_idx = int(args.job_idx % len(learners))
+        task = openml.tasks.get_task(suite.tasks[task_idx])
         if not isinstance(task, openml.tasks.OpenMLSupervisedTask):
             raise ValueError('Can run on Supervised Classification tasks')
-        run_on_task(task, args.output_directory, args.verbose)
+        run_classifier_on_task(learner_idx, task, args.output_directory, args.verbose)
     else:
-        for idx, task_id in enumerate(suite.tasks):
-            try:
-                task = openml.tasks.get_task(task_id)
-                data_name = task.get_dataset().name
-                logging.info('(%d/%d) starting task %d: %s' % (
-                    idx+1, len(suite.tasks), task.task_id, data_name))
-                if not isinstance(task, openml.tasks.OpenMLSupervisedTask):
-                    raise ValueError('Can run on Supervised Classif. tasks')
-                run_on_task(task, args.output_directory, args.verbose)
-            except Exception as e:
-                logging.warning('An exception')
-                print(e)
+        for task_idx, task_id in enumerate(suite.tasks):
+            for learner_idx in range(len(learners)):
+                try:
+                    task = openml.tasks.get_task(task_id)
+                    data_name = task.get_dataset().name
+                    logging.info('(%d/%d) starting task %d: %s' % (
+                        task_idx+1, len(suite.tasks), task.task_id, data_name))
+                    if not isinstance(task, openml.tasks.OpenMLSupervisedTask):
+                        raise ValueError('Can run on Supervised Classif. tasks')
+                    run_classifier_on_task(learner_idx, task, args.output_directory, args.verbose)
+                except Exception as e:
+                    logging.warning('An exception')
+                    print(e)
 
 
 if __name__ == '__main__':
