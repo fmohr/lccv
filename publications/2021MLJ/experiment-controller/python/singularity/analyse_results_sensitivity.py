19009c2c663105f20c1a6d1eb155e889a171ece1
janvanrijn <janvanrijn@gmail.com>
2021-08-23 19:36:51 +0200

    analysise results

--- publications/2021MLJ/experiment-controller/python/singularity/analyse_results_sensitivity.py
+++ publications/2021MLJ/experiment-controller/python/singularity/analyse_results_sensitivity.py
@@ -9,9 +9,8 @@ import pandas as pd
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--result_file', type=str, default=os.path.expanduser('~/results.csv'))
-    parser.add_argument('--dataset_id', type=int, default=None)
-    parser.add_argument('--hyperparameter', type=str, default=None)
     parser.add_argument('--output_dir', type=str, default=os.path.expanduser('~/experiments/lccv_sensitivity'))
+    parser.add_argument('--dimension1', type=str, default='hyperparameter_name')
     parser.add_argument('--extension', type=str, default='png')
     return parser.parse_args()
 
@@ -19,24 +18,37 @@ def parse_args():
 def run(args):
     df = pd.read_csv(args.result_file)
     os.makedirs(args.output_dir, exist_ok=True)
+    if args.dimension1 == 'dataset_id':
+        dimension1 = 'dataset_id'
+        dimension2 = 'hyperparameter_name'
+    elif args.dimension1 == 'hyperparameter_name':
+        dimension1 = 'hyperparameter_name'
+        dimension2 = 'dataset_id'
+    else:
+        raise ValueError()
 
-    hyperparameters = df['hyperparameter_name'].unique()
-    if args.hyperparameter is not None:
-        hyperparameters = args.hyperparameter
-    for hyperparameter in hyperparameters:
+    dimension1_unique_vals = df[dimension1].unique()
+
+    for dimension1_value in dimension1_unique_vals:
         fig, ax = plt.subplots()
         ax.set_xscale('log')
-        frame_hyperparameters = df.loc[df['hyperparameter_name'] == hyperparameter]
-        dids = df['dataset_id'].unique()
-        if args.dataset_id is not None:
-            dids = [args.dataset_id]
-        for did in dids:
-            frame_did = frame_hyperparameters.loc[frame_hyperparameters['dataset_id'] == did]
-            frame_did = frame_did[['hyperparameter_value', 'error_rate', 'runtime']].groupby('hyperparameter_value')
-            ax.plot(frame_did.mean()['runtime'].to_numpy(), frame_did.mean()['error_rate'].to_numpy())
-            print("=== %d === %s ===" % (did, hyperparameter))
-            print(frame_did.mean())
-        filename = os.path.join(args.output_dir, '%s.%s' % (hyperparameter, args.extension))
+        frame_sliced = df.loc[df[dimension1] == dimension1_value]
+        dimension2_unique_vals = df[dimension2].unique()
+
+        for dimension2_value in dimension2_unique_vals:
+            frame_double_sliced = frame_sliced.loc[frame_sliced[dimension2] == dimension2_value]
+            frame_double_sliced = frame_double_sliced[['hyperparameter_value', 'error_rate', 'runtime']].groupby('hyperparameter_value')
+            frame_mean = frame_double_sliced.mean()
+            values = frame_mean.index.to_numpy()
+            runtimes = frame_mean['runtime'].to_numpy()
+            error_rates = frame_mean['error_rate'].to_numpy()
+            ax.plot(runtimes, error_rates, label=dimension2_value)
+            for i in range(len(values)):
+                ax.annotate(values[i], (runtimes[i], error_rates[i]))
+            print("=== %s === %s ===" % (dimension1_value, dimension2_value))
+            print(frame_double_sliced.mean())
+        filename = os.path.join(args.output_dir, '%s.%s' % (dimension1_value, args.extension))
+        plt.legend()
         plt.savefig(filename)
         plt.close(fig)
 
